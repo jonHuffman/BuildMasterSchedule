3bed9822eaaa6e2362d3622d6ad77f2111e6ce19
Jon <huffman.jon@gmail.com>
2020-01-07 09:58:02 -0500

    Created simple app for displaying the scheduled build master

---
+++ Assets/BuildMaster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildMaster : MonoBehaviour
+{
+    public Text date;
+    public Text firstName;
+    public Text secondName;
+    public Text thirdName;
+    public Text fourthName;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TextAsset scheduleJson = Resources.Load<TextAsset>("Schedule");
+        Schedule schedule = JsonUtility.FromJson<Schedule>(scheduleJson.text);
+
+        int weeksSinceStart = (int)((DateTime.Now - schedule.startDate.DateTime).TotalDays / 7);
+        SetBuildMaster(weeksSinceStart, schedule.people);
+
+        date.text = DateTime.Today.ToShortDateString();
+    }
+
+    private void SetBuildMaster(int weeksSinceStart, string[] people)
+    {
+        int buildMasterIndex = weeksSinceStart % people.Length;
+        firstName.text = people[SafeIndex(buildMasterIndex, people.Length)];
+        secondName.text = people[SafeIndex(buildMasterIndex + 1, people.Length)];
+        thirdName.text = people[SafeIndex(buildMasterIndex + 2, people.Length)];
+        fourthName.text = people[SafeIndex(buildMasterIndex + 3, people.Length)];
+    }
+
+    private int SafeIndex(int targetIndex, int maxIndex)
+    {
+        if (targetIndex >= maxIndex)
+        {
+            targetIndex = targetIndex % maxIndex;
+        }
+
+        return targetIndex;
+    }
+}
---
+++ Assets/JsonDateTime.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class JsonDateTime : ISerializationCallbackReceiver
+{
+    [NonSerialized]
+    public DateTime DateTime;
+
+    [SerializeField]
+    private int millisecond;
+    [SerializeField]
+    private int second;
+    [SerializeField]
+    private int minute;
+    [SerializeField]
+    private int hour;
+    [SerializeField]
+    private int day;
+    [SerializeField]
+    private int month;
+    [SerializeField]
+    private int year;
+
+    public JsonDateTime()
+    {
+        DateTime = DateTime.MinValue;
+        OnBeforeSerialize();
+    }
+
+    public JsonDateTime(DateTime dateTime)
+    {
+        DateTime = dateTime;
+        OnBeforeSerialize();
+    }
+
+    public void OnBeforeSerialize()
+    {
+        year = DateTime.Year;
+        month = DateTime.Month;
+        day = DateTime.Day;
+        hour = DateTime.Hour;
+        minute = DateTime.Minute;
+        second = DateTime.Second;
+        millisecond = DateTime.Millisecond;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        DateTime = new DateTime(year, month, day, hour, minute, second, millisecond);
+    }
+
+    public override string ToString()
+    {
+        return DateTime.ToString();
+    }
+}
